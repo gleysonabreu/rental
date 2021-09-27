1f062896d4a45a662a227c79e82fdf5f7d28bf30
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-09-27 18:59:05 -0300

    add entity User

---
+++ src/modules/accounts/models/User.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ name: 'driver_license' })
+  driverLicense: string;
+
+  @Column({ name: 'is_admin' })
+  isAdmin: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
