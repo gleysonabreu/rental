e5576463003d0a7434bfe4fae6b228666ae4f53d
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-09-27 19:23:16 -0300

    fix name folder and imports

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -1,7 +1,7 @@
 import { getRepository, Repository } from 'typeorm';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
-import { User } from '../../models/User';
+import { User } from '../../model/User';
 import { IUsersRepository } from '../IUsersRepository';
 
 class UsersRepository implements IUsersRepository {
