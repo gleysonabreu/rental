ba4fe8c5872f3fc509537737d4ed07cd2d2b72b4
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-09-28 09:43:38 -0300

    add custom errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import './database';
 import './shared/container';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUI from 'swagger-ui-express';
 
+import { AppError } from './errors/AppError';
 import { routes } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -12,5 +14,24 @@ app.use(express.json());
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(routes);
+app.use(
+  (
+    error: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ): Response => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: `Internal Server Error: ${error.message}`,
+    });
+  },
+);
 
 app.listen(3333, () => console.log('Server running'));
