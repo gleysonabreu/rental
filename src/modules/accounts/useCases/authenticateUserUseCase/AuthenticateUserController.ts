def05f6b70c55e5dc852005cda26abe559ace6f4
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-09-28 09:12:59 -0300

    add authentication user

---
+++ src/modules/accounts/useCases/authenticateUserUseCase/AuthenticateUserController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+
+class AuthenticateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { password, email } = request.body;
+
+    const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase);
+    const { user, token } = await authenticateUserUseCase.execute({
+      email,
+      password,
+    });
+
+    return response.json({ user, token });
+  }
+}
+
+export { AuthenticateUserController };
