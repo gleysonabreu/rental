4e574acff82ce2475becfab05f6a554f27785754
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-10-13 09:04:05 -0300

    change connection to execute tests

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,5 +2,13 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (name = 'default'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-  return createConnection(Object.assign(defaultOptions, { name }));
+  return createConnection(
+    Object.assign(defaultOptions, {
+      name,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'rentx_test'
+          : defaultOptions.database,
+    }),
+  );
 };
