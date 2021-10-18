52c6b4e852ab3d213825c0b30e1c18f904471b4a
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-10-18 11:22:03 -0300

    fix upload car images

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import { ICarsImageRepository } from '@modules/cars/repositories/ICarsImageRepository';
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
 
 interface IRequest {
   carId: string;
@@ -12,11 +13,14 @@ class UploadCarImageUseCase {
   constructor(
     @inject('CarsImageRepository')
     private carsImageRepository: ICarsImageRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   async execute({ carId, imageNames }: IRequest): Promise<void> {
     imageNames.map(async imageName => {
       await this.carsImageRepository.create({ carId, imageName });
+      await this.storageProvider.save(imageName, 'cars');
     });
   }
 }
