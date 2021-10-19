9e92bc5d303e4c9cc596751904690bdc3bc2965f
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-10-19 09:36:02 -0300

    add SESMailProvider to send mail with amazon ses.

--- src/shared/container/providers/MailProvider/index.ts
+++ src/shared/container/providers/MailProvider/index.ts
@@ -2,8 +2,14 @@ import { container } from 'tsyringe';
 
 import { IMailProvider } from './IMailProvider';
 import { EtherealMailProvider } from './implementations/EtherealMailProvider';
+import { SESMailProvider } from './implementations/SESMailProvider';
+
+const mailProvider = {
+  ses: container.resolve(SESMailProvider),
+  ethereal: container.resolve(EtherealMailProvider),
+};
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  mailProvider[process.env.MAIL_PROVIDER],
 );
